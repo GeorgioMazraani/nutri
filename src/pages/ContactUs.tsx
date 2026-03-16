7020fac3c19631c41bfcd494b3f15fb23950a375
GeorgioMazraani <mazraanigeorgio@gmail.com>
2026-03-16 15:38:27 +0200

    test

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -87,18 +87,6 @@ const contactInfo = [
   import.meta.env.VITE_EMAILJS_PUBLIC_KEY
 );
 
-      await emailjs.send(
-  import.meta.env.VITE_EMAILJS_SERVICE_ID,
-  import.meta.env.VITE_EMAILJS_TEMPLATE_AUTOREPLY,
-  {
-    name: form.name,
-    email: form.email,
-    subject: form.subject,
-    message: form.message,
-  },
-  import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-);
-
       setSubmitted(true);
       setForm(initialForm);
       setErrors({});
